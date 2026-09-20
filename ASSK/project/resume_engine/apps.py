1a8e7c8b91c90f88c2ffcf170847cb21da2823e8
Sai Santhosh Kumar Akula <116353787+assk2005@users.noreply.github.com>
2026-09-20 12:36:10 +0530

    Delete ASSK directory

--- ASSK/project/resume_engine/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class ResumeEngineConfig(AppConfig):
-    name = 'resume_engine'
--- ASSK/project/resume_engine/services/scoring_engine.py
+++
@@ -1,6 +0,0 @@
-def compute_final_score(skill_score,experience_score,config):
-    final_score=(
-        config.skill_weight*skill_score+
-        config.experience_weight*experience_score
-    )
-    return final_score
